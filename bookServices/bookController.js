fe9820e4c3e40954d269c9e8af01601d73b8b343
Shruti0026 <naik.shrutika026@gmail.com>
2023-01-27 16:48:45 +0530

    code changed according to mongo connection

--- bookServices/bookController.js
+++ bookServices/bookController.js
@@ -1,85 +1,50 @@
-const fs = require("fs")
-const dataPath = './ManagementData/books.json'
-const data = fs.readFileSync('./ManagementData/books.json')
+// const fs = require("fs")
+// const dataPath = './ManagementData/books.json'
+// const data = fs.readFileSync('./ManagementData/books.json')
 
+const { Model } = require("mongoose");
+//const book = require("./Book")
+const Book = require('./Book')
+
+const addNewBook = async(req, res) => {
+    const book = new Book({
+        Title: req.body.Title,
+        Description: req.body.Description,
+        Author: req.body.Author
+    })
+    try {
+        const newBook = await book.save()
+        res.status(200).json(newBook)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+const getBooksList = async(req, res) => {
+    try {
+        const books = await Book.find()
+        res.json(books)
+        console.log("inside find router")
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+}
+
+const deleteBook = async(req, res) => {
+    try {
+        const id = req.params.id;
+        console.log(id)
+        const data = await Book.findByIdAndDelete(id);
+        res.send(` deleted..`)
+        console.log("data found")
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+        console.log("not data found")
+    }
+}
 
 module.exports = {
-    getBooksList: (req, res) => {
-        fs.readFile('./ManagementData/books.json', 'utf8', function(err, data) {
-
-            if (err) {
-                return console.error(err);
-            }
-            data = JSON.parse(data, null, 2);
-            res.end(JSON.stringify(data));
-        })
-    },
-
-    addNewBook: (req, res) => {
-        const book = {
-            "id": req.body.id,
-            "title": req.body.title,
-            "Description": req.body.Description,
-            "Author": req.body.Author
-        }
-        const rawdata = fs.readFileSync('./ManagementData/books.json')
-        const jsonData = JSON.parse(rawdata)
-        jsonData.push(book)
-        const data = JSON.stringify(jsonData, null, 2)
-        fs.writeFileSync('./ManagementData/books.json', data, (err, res) => {
-            if (err)
-                throw err;
-            console.log("Record inserted Successfully");
-
-        });
-        res.send(JSON.parse(data));
-    },
-
-    updateRecord: (req, res) => {
-        //console.log(req.body)
-        const bookId = req.body['id']
-        console.log("id", bookId)
-        const rawdata = fs.readFileSync('./ManagementData/books.json')
-        const jsonData = JSON.parse(rawdata)
-
-        const data = JSON.stringify(jsonData, null, 2)
-            //console.log("type ", typeof data)
-        var found = jsonData.findIndex(obj => obj.id === bookId)
-
-        if (!found) {
-
-            return res.send("Check ID")
-        }
-        let update = {
-            id: req.body.id,
-            Title: req.body.Title,
-            Description: req.body.Description,
-            Author: req.body.Author
-        }
-        jsonData.splice(found, 1, update)
-        const newdata = JSON.stringify(jsonData, null, 2)
-        fs.writeFileSync('./ManagementData/books.json', newdata, (err, res) => {
-            if (err)
-                throw (err.name, err.message);
-            res.status(201)
-        });
-        res.send(JSON.parse(newdata))
-
-    },
-
-    deleteBookRecord: (req, res) => {
-        const bookId = req.body['id']
-        const rawdata = fs.readFileSync('./ManagementData/books.json')
-        const jsonData = JSON.parse(rawdata)
-        const filteredDataIndex = jsonData.map(function(b) { return b.id; }).indexOf(bookId);
-        jsonData.splice(filteredDataIndex, 1)
-        const data = JSON.stringify(jsonData, null, 2)
-        fs.writeFileSync('./ManagementData/books.json', data, (err, res) => {
-            if (err)
-                throw err;
-            console.log("Record inserted Successfully");
-        });
-        res.send(JSON.parse(data));
-    }
-
+    addNewBook,
+    getBooksList,
+    deleteBook
 }
--- bookServices/bookRouter.js
+++ bookServices/bookRouter.js
@@ -1,14 +1,135 @@
-const { getBooksList, addNewBook, deleteBookRecord, updateRecord } = require('./bookController')
-const fs = require("fs")
-const router = require("express").Router();
+const { addNewBook, getBooksList, deleteBook } = require('./bookController')
+    //const fs = require("fs")
+const express = require("express");
+const app = express.Router();
+const Book = require('./Book')
 
-router.get("/getBooksList", getBooksList)
-router.post("/addNewBook", addNewBook)
-router.delete("/deleteBook", deleteBookRecord)
-router.put("/updateRecord", updateRecord)
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      Books:
+ *        type: object
+ *        properties:
+ *          Title:
+ *            type: string
+ *            description: enter book title
+ *          Description:
+ *            type: string
+ *            description: enter book description
+ *          Author:
+ *            type: string
+ *            description: enter author name
+ */
 
 
+/**
+ * @swagger
+ * /get:
+ *  get:
+ *      summary: this api is to check if get method is working
+ *      description: this api is to check if get method is working
+ *      responses: 
+ *          200:
+ *              description: to test get method
+ */
+app.get("/get", (req, res) => {
+    console.log("inside router")
+    res.send("welcome to Library Management System")
+})
 
 
+/**
+ * @swagger
+ * /book/getBooksList:
+ *  get:
+ *    tags:
+ *      - Books
+ *    summary: Retrieve a list of books
+ *    description: Retrieve a list of books from mongodb
+ *    responses:
+ *      200:
+ *        description: A list of books.
+ *        content: 
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                description:
+ *                  type: string
+ *                  example: Successfully fetched all data!
+ *                data:
+ *                  type: array
+ *                  items:
+ *                    $ref: '#/components/schemas/Books'
+ */
+app.get("/getBooksList", getBooksList);
 
-module.exports = router;
+/**
+ * @swagger
+ * /book/addNewBook:
+ *    post:
+ *      tags:
+ *        - Books
+ *      description: Add new Book
+ *      summary: Create book data
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *               Title:
+ *                  type: string
+ *                  description: enter book title
+ *               Description:
+ *                  type: string
+ *                  description: enter book description
+ *               Author:
+ *                  type: string
+ *                  description: enter author name
+ *      responses:
+ *        200:
+ *          description: Successfully created data
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  description:
+ *                    type: string
+ *                    example: Successfully created data! 
+ */
+app.post("/addNewBook", addNewBook)
+
+
+/**
+ * @swagger
+ * /book/delete/{id}:
+ *    delete:
+ *      tags:
+ *        - Books
+ *      summary: Remove book data by id
+ *      description: Remove book API  
+ *      parameters:
+ *        - name: id
+ *          in: path
+ *          required: true
+ *          description: Books id
+ *      responses:
+ *        200:
+ *          description: Successfully deleted data
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  description:
+ *                    type: string
+ *                    example: Successfully updated data!     
+ */
+app.delete("/delete/:id", deleteBook)
+
+
+module.exports = app;
